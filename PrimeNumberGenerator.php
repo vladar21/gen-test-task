1b35597ff47098f4ed49078c583e66c6c2050885
vladar21 <vladar21@gmail.com>
2024-01-17 20:48:23 +0000

    add PrimeNumberGenerator class

---
+++ PrimeNumberGenerator.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace MyPrimeApp;
+
+class PrimeNumberGenerator
+{
+    /**
+     * Generate the first N prime numbers.
+     *
+     * @param int $n The number of prime numbers to generate.
+     * @return int[] An array containing the first N prime numbers.
+     */
+    public function generatePrimes(int $n): array
+    {
+        $primes = [];
+        $candidate = 2;
+
+        while (count($primes) < $n) {
+            if ($this->isPrime($candidate)) {
+                $primes[] = $candidate;
+            }
+            $candidate++;
+        }
+
+        return $primes;
+    }
+
+    /**
+     * Check if a number is prime.
+     *
+     * @param int $number The number to check.
+     * @return bool True if the number is prime, false otherwise.
+     */
+    private function isPrime(int $number): bool
+    {
+        if ($number <= 1) {
+            return false;
+        }
+
+        for ($i = 2; $i * $i <= $number; $i++) {
+            if ($number % $i === 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
